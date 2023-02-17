bdfcb53c5fd92f0dbc60ded814be46dc5d7bdb63
DikshaAurobindo <124631681+DikshaAurobindo@users.noreply.github.com>
2023-02-17 09:58:10 -0500

    Update functions.js

--- functions.js
+++ functions.js
@@ -65,4 +65,12 @@ document.onkeydown = function (e) {
     myEvent =  e || window.event; 
 };
 
+<!-- Google tag (gtag.js) -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-W9B5QPV3BH"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
 
+  gtag('config', 'G-W9B5QPV3BH');
+</script>
